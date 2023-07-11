8a0a55cc3b1828d0b4876eb03fe6e0e781b0c18e
mbakcn <mahesh.baraskar@scania.com>
2023-07-11 11:58:17 +0200

    adding image

--- src/App.js
+++ src/App.js
@@ -89,6 +89,13 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+
+          <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
+          />
           <Button type="submit" variation="primary">
             Create Note
           </Button>
@@ -120,12 +127,6 @@ const App = ({ signOut }) => {
           </Flex>
         ))}
       </View>
-      <View
-        name="image"
-        as="input"
-        type="file"
-        style={{ alignSelf: "end" }}
-      />
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
